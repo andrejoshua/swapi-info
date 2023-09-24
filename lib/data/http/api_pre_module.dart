8955f4b19523ca4e239afd5a99f9c7bf4d5bb954
andrejoshua <yosua.andre@gmail.com>
2023-09-24 23:51:22 +0800

    Added caching mechanism

--- lib/data/http/api_pre_module.dart
+++ lib/data/http/api_pre_module.dart
@@ -1,15 +1,51 @@
+import "dart:io";
+
 import "package:dio/dio.dart";
+import "package:dio_cache_interceptor/dio_cache_interceptor.dart";
+import "package:dio_cache_interceptor_file_store/dio_cache_interceptor_file_store.dart";
 import "package:flutter/foundation.dart";
 import "package:injectable/injectable.dart";
+import "package:path_provider/path_provider.dart";
 
 import "constants.dart";
 import "status_code_ext.dart";
 
 @module
 abstract class ApiPreModule {
+  @preResolve
+  @Named(kScopeTemporaryDir)
+  Future<String> getCacheDirectoryPath() {
+    return getTemporaryDirectory().then((Directory value) => value.path);
+  }
 
   @injectable
-  Dio getDio() {
+  DioCacheInterceptor getCacheInterceptor(
+      @Named(kScopeTemporaryDir) String cacheDirectoryPath) {
+    // Global options
+    final CacheOptions options = CacheOptions(
+      // Uses file cache store, since the cache makes more sense to be saved in temporary storage
+      // There is another option, using MemCacheStore, but it's saved on runtime, which will be wiped out once the app is terminated
+      store: FileCacheStore(cacheDirectoryPath),
+      // Standard policy
+      policy: CachePolicy.request,
+      // Overrides any HTTP directive to delete entry past this duration.
+      // Useful only when origin server has no cache config or custom behaviour is desired.
+      maxStale: const Duration(days: 7),
+      // Allows 3 cache sets and ease cleanup.
+      priority: CachePriority.normal,
+      // Body and headers encryption with your own algorithm.
+      cipher: null,
+      // Key builder to retrieve requests.
+      keyBuilder: CacheOptions.defaultCacheKeyBuilder,
+      // Allows to cache POST requests.
+      // Overriding [keyBuilder] is strongly recommended when [true].
+      allowPostMethod: false,
+    );
+    return DioCacheInterceptor(options: options);
+  }
+
+  @lazySingleton
+  Dio getDio(DioCacheInterceptor cacheInterceptor) {
     final BaseOptions options = BaseOptions(
       baseUrl: kBaseUrl,
       followRedirects: false,
@@ -20,6 +56,7 @@ abstract class ApiPreModule {
       sendTimeout: kDefaultSendTimeout,
     );
     final Dio dio = Dio(options);
+    dio.interceptors.add(cacheInterceptor);
 
     if (kDebugMode) {
       dio.interceptors
--- lib/di.config.dart
+++ lib/di.config.dart
@@ -9,62 +9,71 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:dio/dio.dart' as _i3;
+import 'package:dio/dio.dart' as _i5;
+import 'package:dio_cache_interceptor/dio_cache_interceptor.dart' as _i4;
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 
-import 'data/api/service.dart' as _i6;
-import 'data/feature/repository.dart' as _i8;
-import 'data/http/api_pre_module.dart' as _i16;
-import 'data/http/client.dart' as _i5;
-import 'domain/repository/repository.dart' as _i7;
-import 'domain/usecase/get_character_by_id.dart' as _i9;
-import 'domain/usecase/get_characters.dart' as _i10;
-import 'domain/usecase/get_homeworld_by_id.dart' as _i11;
-import 'domain/usecase/get_starships_by_ids.dart' as _i12;
-import 'domain/usecase/get_vehicles_by_ids.dart' as _i13;
-import 'domain/usecase/search_character_by_name.dart' as _i4;
-import 'ui/character_detail/vm.dart' as _i14;
-import 'ui/characters/vm.dart' as _i15;
+import 'data/api/service.dart' as _i7;
+import 'data/feature/repository.dart' as _i9;
+import 'data/http/api_pre_module.dart' as _i17;
+import 'data/http/client.dart' as _i6;
+import 'domain/repository/repository.dart' as _i8;
+import 'domain/usecase/get_character_by_id.dart' as _i10;
+import 'domain/usecase/get_characters.dart' as _i11;
+import 'domain/usecase/get_homeworld_by_id.dart' as _i12;
+import 'domain/usecase/get_starships_by_ids.dart' as _i13;
+import 'domain/usecase/get_vehicles_by_ids.dart' as _i14;
+import 'domain/usecase/search_character_by_name.dart' as _i3;
+import 'ui/character_detail/vm.dart' as _i15;
+import 'ui/characters/vm.dart' as _i16;
 
 // initializes the registration of main-scope dependencies inside of GetIt
-_i1.GetIt $initGetIt(
+Future<_i1.GetIt> $initGetIt(
   _i1.GetIt getIt, {
   String? environment,
   _i2.EnvironmentFilter? environmentFilter,
-}) {
+}) async {
   final gh = _i2.GetItHelper(
     getIt,
     environment,
     environmentFilter,
   );
   final apiPreModule = _$ApiPreModule();
-  gh.factory<_i3.Dio>(() => apiPreModule.getDio());
-  gh.factory<_i4.SearchCharactersByName>(() => _i4.SearchCharactersByName());
-  gh.factory<_i5.ApiClient>(() => _i5.ApiClient(gh<_i3.Dio>()));
-  gh.factory<_i6.ApiService>(() => _i6.ApiService(gh<_i5.ApiClient>()));
-  gh.factory<_i7.Repository>(() => _i8.RepositoryImpl(gh<_i6.ApiService>()));
-  gh.factory<_i9.GetCharacterByIdUseCase>(
-      () => _i9.GetCharacterByIdUseCase(gh<_i7.Repository>()));
-  gh.factory<_i10.GetCharactersUseCase>(
-      () => _i10.GetCharactersUseCase(gh<_i7.Repository>()));
-  gh.factory<_i11.GetHomeworldByIdUseCase>(
-      () => _i11.GetHomeworldByIdUseCase(gh<_i7.Repository>()));
-  gh.factory<_i12.GetStarshipsByIdsUseCase>(
-      () => _i12.GetStarshipsByIdsUseCase(gh<_i7.Repository>()));
-  gh.factory<_i13.GetVehiclesByIdsUseCase>(
-      () => _i13.GetVehiclesByIdsUseCase(gh<_i7.Repository>()));
-  gh.factory<_i14.CharacterDetailViewModel>(() => _i14.CharacterDetailViewModel(
-        gh<_i9.GetCharacterByIdUseCase>(),
-        gh<_i13.GetVehiclesByIdsUseCase>(),
-        gh<_i12.GetStarshipsByIdsUseCase>(),
-        gh<_i11.GetHomeworldByIdUseCase>(),
+  gh.factory<_i3.SearchCharactersByName>(() => _i3.SearchCharactersByName());
+  await gh.factoryAsync<String>(
+    () => apiPreModule.getCacheDirectoryPath(),
+    instanceName: 'TEMPORARY_DIRECTORY',
+    preResolve: true,
+  );
+  gh.factory<_i4.DioCacheInterceptor>(() => apiPreModule
+      .getCacheInterceptor(gh<String>(instanceName: 'TEMPORARY_DIRECTORY')));
+  gh.lazySingleton<_i5.Dio>(
+      () => apiPreModule.getDio(gh<_i4.DioCacheInterceptor>()));
+  gh.factory<_i6.ApiClient>(() => _i6.ApiClient(gh<_i5.Dio>()));
+  gh.factory<_i7.ApiService>(() => _i7.ApiService(gh<_i6.ApiClient>()));
+  gh.factory<_i8.Repository>(() => _i9.RepositoryImpl(gh<_i7.ApiService>()));
+  gh.factory<_i10.GetCharacterByIdUseCase>(
+      () => _i10.GetCharacterByIdUseCase(gh<_i8.Repository>()));
+  gh.factory<_i11.GetCharactersUseCase>(
+      () => _i11.GetCharactersUseCase(gh<_i8.Repository>()));
+  gh.factory<_i12.GetHomeworldByIdUseCase>(
+      () => _i12.GetHomeworldByIdUseCase(gh<_i8.Repository>()));
+  gh.factory<_i13.GetStarshipsByIdsUseCase>(
+      () => _i13.GetStarshipsByIdsUseCase(gh<_i8.Repository>()));
+  gh.factory<_i14.GetVehiclesByIdsUseCase>(
+      () => _i14.GetVehiclesByIdsUseCase(gh<_i8.Repository>()));
+  gh.factory<_i15.CharacterDetailViewModel>(() => _i15.CharacterDetailViewModel(
+        gh<_i10.GetCharacterByIdUseCase>(),
+        gh<_i14.GetVehiclesByIdsUseCase>(),
+        gh<_i13.GetStarshipsByIdsUseCase>(),
+        gh<_i12.GetHomeworldByIdUseCase>(),
       ));
-  gh.factory<_i15.CharactersViewModel>(() => _i15.CharactersViewModel(
-        gh<_i10.GetCharactersUseCase>(),
-        gh<_i4.SearchCharactersByName>(),
+  gh.factory<_i16.CharactersViewModel>(() => _i16.CharactersViewModel(
+        gh<_i11.GetCharactersUseCase>(),
+        gh<_i3.SearchCharactersByName>(),
       ));
   return getIt;
 }
 
-class _$ApiPreModule extends _i16.ApiPreModule {}
+class _$ApiPreModule extends _i17.ApiPreModule {}
