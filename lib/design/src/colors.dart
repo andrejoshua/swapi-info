ec60ee3d5a651966df76b0da5e08958661b7f51b
andrejoshua <yosua.andre@gmail.com>
2023-09-25 15:47:32 +0800

    Added documentation and setup for iOS build

--- lib/design/src/colors.dart
+++ lib/design/src/colors.dart
@@ -1,5 +1,6 @@
 import "package:flutter/material.dart";
 
+const Color kColorPrimary = Color(0xFFB71C1C);
 const Color kColorBackground = Colors.white;
 const Color kColorCharacterBackground = Color(0xFFC4C4C4);
 const Color kColorStarshipBackground = Color(0xFFFFF3DD);
@@ -16,14 +17,14 @@ const Color kColorDarkGray = Color(0xFF676767);
 // Sample of using color scheme
 // Now we will only use light theme
 const ColorScheme kSchemeLight = ColorScheme.light(
-  primary: kColorCharacterBackground,
-  onPrimary: kColorText,
-  secondary: kColorStarshipBackground,
-  onSecondary: kColorText,
-  tertiary: kColorVehicleBackground,
-  onTertiary: kColorText,
+  // Primary color
+  primary: kColorPrimary,
+  // Color above primary
+  onPrimary: kColorTextWhite,
+  // Default additional color, set to grey
   surface: kColorBackground,
-  onSurface: kColorText,
+  onSurface: kColorTextWhite,
+  // Default background color, set to white
   background: kColorBackground,
   onBackground: kColorText,
   error: kColorError,
